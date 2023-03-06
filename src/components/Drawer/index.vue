d941c8cbf103919f01aa9ade7cfe38b1be7c54a8
longphamnam99 <longkul1478@gmail.com>
2023-03-06 16:35:24 +0700

    add category form

---
+++ src/components/Drawer/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <el-drawer
+      :title="title"
+      :visible.sync="drawer"
+      :direction="direction"
+      :before-close="handleClose"
+    >
+      <slot />
+    </el-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    position: {
+      type: String,
+      default: 'rtl'
+    },
+    visible: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      direction: this.position,
+      drawer: this.visible
+    }
+  },
+  methods: {
+    handleClose(done) {
+      this.$confirm('Bạn có chắc sẽ đóng tác vụ hiện tại?')
+        .then((_) => {
+          done()
+        })
+        .catch((_) => {})
+    }
+  }
+}
+</script>
--- src/views/products/category/index.vue
+++ src/views/products/category/index.vue
@@ -1,15 +1,35 @@
 <template>
   <div class="app-container">
+    <div class="filter-container">
+      <el-row>
+        <div class="text-right">
+          <el-button icon="el-icon-refresh-left">
+            Tải lại
+          </el-button>
+          <el-button icon="el-icon-circle-plus" type="success" @click="showFormAdd = true">
+            Thêm
+          </el-button>
+        </div>
+      </el-row>
+    </div>
+    <drawer title="Thêm danh mục" :visible.sync="showFormAdd" />
     <List />
   </div>
 </template>
 
 <script>
 import List from './list.vue'
+import Drawer from '@/components/Drawer'
 
 export default {
   components: {
+    Drawer,
     List
+  },
+  data() {
+    return {
+      showFormAdd: false
+    }
   }
 }
 </script>
